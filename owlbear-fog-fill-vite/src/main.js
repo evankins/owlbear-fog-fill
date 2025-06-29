6334ecdcaebfc932ae644a1c20ddc372b18115f3
evankins <112184136+evankins@users.noreply.github.com>
2025-06-28 22:29:23 -0400

    initial setup: get scene info and place fog

--- owlbear-fog-fill-vite/src/main.js
+++ owlbear-fog-fill-vite/src/main.js
@@ -1,24 +1,90 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import OBR, { buildWall, buildCurve } from "@owlbear-rodeo/sdk";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const ID = "com.evankinsey.fog-fill";
 
-setupCounter(document.querySelector('#counter'))
+// subscribe to scene changes to clear the local copy of scene?
+let scene;
+
+function createMode() {
+  OBR.tool.createMode({
+    id: `${ID}/tool`,
+    icons: [
+      {
+        icon: "/add.svg",
+        label: "Fog Fill",
+        // Only show if fog tool is open
+        filter: {
+          activeTools: [`rodeo.owlbear.tool/fog`],
+        },
+      },
+    ],
+    async onToolClick(context, event) {
+      // Get the position of the pointer
+      const pointerPos = event.pointerPosition;
+
+      // Relate that position to a point on the map
+      //    - Prompt user for map if not given
+      //    - Image url expires shortly after given
+      if (!scene)
+      {
+        scene = await OBR.assets.downloadScenes(false);
+      }
+      console.log('scene --->', scene);
+
+      const imageSize = {
+        x: scene[0].items[0].image.width * 3,
+        y: scene[0].items[0].image.height * 3
+      };
+
+      
+      console.log('imageSize --->', imageSize);
+      console.log('pointerPos --->', pointerPos);
+
+      // Ignore if out of bounds
+      //    - Either value is negative or larger than map size
+      //    - Send message if ignored
+      //    - My current observation is x/y is 3 times the size of 
+      //      original map uploaded, what determines that?
+
+      // Exit if invalid mouse position
+      if (pointerPos.x > imageSize.x ||
+          pointerPos.y > imageSize.y ||
+          pointerPos.x < 0 ||
+          pointerPos.y < 0
+      )
+      {
+        console.log("Bad")
+        return; // Early return, invalid mouse position
+      }
+        
+      console.log("Good")
+
+      // Run a fill algorithm that returns an array of points to 
+      // create a polygon
+
+      // Create an owlbear rodeo fog polygon 
+      
+      const fog = buildCurve()
+      .points([
+        { x: pointerPos.x, y: pointerPos.y },
+        { x: 150, y: 0 },
+        { x: 150, y: 150 },
+      ])
+      .tension(0)
+      .layer("FOG")
+      .build();
+
+      // Place that fog onto the map
+      OBR.scene.items.addItems([fog]);
+    }
+  });
+}
+
+async function getInfo() {
+  console.log(await OBR.scene.local.getItems())
+}
+
+OBR.onReady(() => {
+  createMode();
+  getInfo();
+});
